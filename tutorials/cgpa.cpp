c6d4a58d479ce3e24e2a50cbf00a0afff05628d6
hussain-isa <hussainisadanfillo@gmail.com>
2026-01-04 15:25:57 +0100

    Auto update: Sun Jan  4 03:25:57 PM WAT 2026

--- tutorials/cgpa.cpp
+++ tutorials/cgpa.cpp
@@ -3,45 +3,56 @@ using namespace std;
 
 int main()
 {
-    float cgpa;
-    string grade;
-    string subject;
-    int numSubjects, credits;
+    int numSubjects;
+    int credits;
+    float gradePoint;
+    float totalPoints = 0;
+    int totalCredits = 0;
+    string grade, subject;
 
-
-    cout << "Enter the subject: ";
-    cin >> subject;
-
-    cout << "Enter the Number of subjects: ";
+    cout << "Enter number of subjects: ";
     cin >> numSubjects;
 
-    cout << "Enter the number of credits (1-4): ";
-    cin >> credits;
-
-    if (credits <= 0 || credits > 4)
+    for (int i = 1; i <= numSubjects; i++)
     {
-        cout << "Invalid number of credits" << endl;
-        return 0;
-    }
-    
+        cout << "\nEnter subject name: ";
+        cin >> subject;
 
-    cout << "Enter the Grade: ";
-    cin >> grade;
-    if (grade == "A" || grade == "a")
-        cgpa = 4.0;
-    else if (grade == "B" || grade == "b")
-        cgpa = 3.0;
-    else if (grade == "C" || grade == "c")
-        cgpa = 2.0;
-    else if (grade == "D" || grade == "d")
-        cgpa = 1.0;
-    else if (grade == "F" || grade == "f")
-        cgpa = 0.0;
-    else
-    {
-        cout << "Invalid Grade Input" << endl;
-        return 0;
+        cout << "Enter credits (1-4): ";
+        cin >> credits;
+
+        if (credits <= 0 || credits > 4)
+        {
+            cout << "Invalid credit value!" << endl;
+            return 0;
+        }
+
+        cout << "Enter grade (A-F): ";
+        cin >> grade;
+
+        if (grade == "A" || grade == "a")
+            gradePoint = 4.0;
+        else if (grade == "B" || grade == "b")
+            gradePoint = 3.0;
+        else if (grade == "C" || grade == "c")
+            gradePoint = 2.0;
+        else if (grade == "D" || grade == "d")
+            gradePoint = 1.0;
+        else if (grade == "F" || grade == "f")
+            gradePoint = 0.0;
+        else
+        {
+            cout << "Invalid grade!" << endl;
+            return 0;
+        }
+
+        totalPoints += gradePoint * credits;
+        totalCredits += credits;
     }
-    cout << "Your CGPA: " << cgpa * credits / credits << endl;
+
+    float cgpa = totalPoints / totalCredits;
+
+    cout << "\nYour CGPA is: " << cgpa << endl;
+
     return 0;
 }
