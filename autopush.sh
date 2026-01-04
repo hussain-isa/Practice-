e3f4d71c50a8222eb128c58cfc5bcb181e5f33fe
hussain-isa <hussainisadanfillo@gmail.com>
2026-01-04 15:27:59 +0100

    Auto update: Sun Jan  4 03:27:59 PM WAT 2026

--- autopush.sh
+++ autopush.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-while true
-do
-  git add .
-  
-  if git diff --cached --quiet; then
-    echo "No changes to commit"
-  else
-    git commit -m "Auto update: $(date)"
-    git push origin main
-    echo "Changes pushed to GitHub"
-  fi
 
-  sleep 60
-done
